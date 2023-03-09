5db178ef05a6a4a75549037e7941c46405adee89
niklierik <erik200203@gmail.com>
2023-03-09 13:40:03 +0100

    Connected to DB

--- server/src/db/db.ts
+++
@@ -1,9 +0,0 @@
-import { cfg } from "src/config/config";
-import { DataSource } from "typeorm";
-
-export const AppDataSource = new DataSource({
-  type: "oracle",
-  name: cfg().db.name,
-  host: "localhost",
-  port: 2,
-});
