3210c326c269ca737ddf714746d0eb1c16524a8b
niklierik <erik200203@gmail.com>
2023-04-13 16:28:37 +0200

    Fixed syntax errors

--- server/src/sql/rooms/room-size-validation-trigger.sql
+++ server/src/sql/rooms/room-size-validation-trigger.sql
@@ -1,8 +1,10 @@
+-- Validates whether a room has a valid size / capacity.
 CREATE OR REPLACE TRIGGER RoomSizeValidation
 BEFORE INSERT OR UPDATE
 ON "SYSTEM"."rooms"
+FOR EACH ROW
 BEGIN
-    IF NEW.size <= 0 THEN
-        RAISE_APPLICATION_EXCEPTION(-20001, 'Room size validation failed. Must be positive integer.');
-    ENDIF;
+    IF :NEW."size" <= 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Room size validation failed. Must be positive integer.');
+    END IF;
 END;
