76f0874e93ee95c8aa23d30ea3fb742124b923c1
niklierik <erik200203@gmail.com>
2023-04-13 16:33:19 +0200

    Fixed syntax errors

---
+++ server/src/sql/users/user-validation-trigger.sql
@@ -0,0 +1,15 @@
+-- Validates user's data (mostly empty string check)
+CREATE OR REPLACE TRIGGER UserValidationTrigger
+BEFORE INSERT OR UPDATE
+ON "SYSTEM"."users"
+FOR EACH ROW
+BEGIN
+    IF  (:NEW."email" NOT LIKE '%@%') OR
+        (LENGTH(:NEW."address") = 0) OR
+        (LENGTH(:NEW."forename") = 0) OR
+        (LENGTH(:NEW."familyname") = 0) OR
+        (LENGTH(:NEW."isAdmin") NOT IN (0, 1)) THEN
+        -- MajorID is checked by DB with foreign key check
+            RAISE_APPLICATION_ERROR(-20001, 'User validation failed.');
+    END IF;
+END;
