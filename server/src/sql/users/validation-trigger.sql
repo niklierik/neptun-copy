65a6623592e57cdb6282c782eae25eaf257bcd45
niklierik <erik200203@gmail.com>
2023-04-13 14:36:10 +0200

    Then was missing

--- server/src/sql/users/validation-trigger.sql
+++ server/src/sql/users/validation-trigger.sql
@@ -7,7 +7,7 @@ BEGIN
         (LENGTH(NEW.address) == 0) OR
         (LENGTH(NEW.forename) == 0) OR
         (LENGTH(NEW.familyname) == 0) OR
-        (LENGTH(NEW.isAdmin) NOT IN (0, 1))
+        (LENGTH(NEW.isAdmin) NOT IN (0, 1)) THEN
         -- MajorID is checked by DB with foreign key check
             RAISE_APPLICATION_ERROR(-20001, "User validation failed.")
     ENDIF;
