9249c63513729aee7a00b1d17287b25b1c9ee234
niklierik <erik200203@gmail.com>
2023-04-16 13:09:53 +0200

    Fixed import modules and added join course

--- server/src/courses/courses.service.ts
+++ server/src/courses/courses.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "src/users/entities/users.entity";
 import { CoursesRepository } from "./courses.repository";
 import { Course } from "./entities/course.entity";
 
@@ -6,7 +7,10 @@ import { Course } from "./entities/course.entity";
 export class CoursesService {
   constructor(private readonly coursesRepository: CoursesRepository) {}
 
-  async list() {
+  async list(user: User) {
+    if (!user.isAdmin) {
+      return this.getCourses(user.email);
+    }
     return this.coursesRepository.find({
       relations: {
         room: true,
@@ -22,5 +26,13 @@ export class CoursesService {
   async getCourses(user: string): Promise<Course[]> {
     const courses = await this.coursesRepository.findFor(user);
     return courses;
+  }
+
+  async joinCourse(user: User, id: string) {
+    const course = await this.coursesRepository.findOne({
+      where: { id: id },
+    });
+    course.students.push(user);
+    return this.coursesRepository.save(course);
   }
 }
--- server/src/studies/studies.module.ts
+++ server/src/studies/studies.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { MarksModule } from "src/marks/marks.module";
 import { StudiesController } from "./studies.controller";
 import { StudiesService } from "./studies.service";
 
 @Module({
+  imports: [MarksModule],
   controllers: [StudiesController],
   providers: [StudiesService],
 })
