7adeca610b9cca08aeeb7b0f3894bf04eebdd6bc
niklierik <erik200203@gmail.com>
2023-04-20 18:04:44 +0200

    Added landing page service and model

---
+++ client/src/common/services/landing-page.service.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { LandingPage } from "../models/landing-page";
+import { getServerUrl } from "../cfg";
+import { getAuthToken } from "../utils";
+
+export class LandingPageService {
+    static async get(): Promise<LandingPage> {
+        const res = await axios.get<LandingPage>(getServerUrl("landing-page"), {
+            headers: { Authorization: getAuthToken() },
+        });
+        return res.data;
+    }
+}
