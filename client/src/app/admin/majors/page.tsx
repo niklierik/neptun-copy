a2f030f008c792dbd6920fcc70d1966e5b8e027f
niklierik <erik200203@gmail.com>
2023-04-22 17:25:17 +0200

    Add majors

--- client/src/app/admin/majors/page.tsx
+++ client/src/app/admin/majors/page.tsx
@@ -3,38 +3,93 @@
 import Header from "@/common/header";
 import { Form, Button } from "react-bootstrap";
 import { MajorsTable } from "../datas/majors/majors-table";
-import MajorsData from "../datas/majors/page";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import axios from "axios";
+import { getServerUrl } from "@/common/cfg";
+import { getAuthToken, handleError } from "@/common/utils";
+
+export type StringState = [string, Dispatch<SetStateAction<string>>];
+export type StringArrayState = [string[], Dispatch<SetStateAction<string[]>>];
+
+export function submitMajors(
+    event: FormEvent<HTMLFormElement>,
+    name: StringState,
+    short: StringState,
+    err: StringArrayState,
+) {
+    event.preventDefault();
+    axios
+        .post(
+            getServerUrl("majors"),
+            {
+                displayName: name[0],
+                majorID: short[0],
+            },
+            { headers: { Authorization: getAuthToken() } },
+        )
+        .then(() => {
+            window.location.href = "/admin/majors";
+        })
+        .catch((e) => handleError(e, err[1]));
+}
 
 export default function Majors() {
+    const nameState = useState("");
+    const shortState = useState("");
+    const errState = useState<string[]>([]);
+    return (
+        <main>
+            <Header></Header>
 
-    return <main>
+            <div className="to_center border_3px">
+                <Form
+                    className="format"
+                    onSubmit={(event) =>
+                        submitMajors(event, nameState, shortState, errState)
+                    }
+                >
+                    <Form.Group
+                        className="mb-3 form_group"
+                        controlId="formBasicText"
+                    >
+                        <Form.Label>Szak neve</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Név"
+                            value={nameState[0]}
+                            onChange={(event) => {
+                                nameState[1](event.target.value);
+                            }}
+                        />
+                    </Form.Group>
 
-        <Header></Header>
+                    <Form.Group className="mb-3 form_group">
+                        <Form.Label>Szak rövidítése</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Rövidítés"
+                            onChange={(event) => {
+                                shortState[1](event.target.value);
+                            }}
+                            value={shortState[0]}
+                        />
+                    </Form.Group>
 
-        <div className="to_center border_3px">
-            <Form className="format">
-                <Form.Group className="mb-3 form_group" controlId="formBasicText">
-                    <Form.Label>Szak neve</Form.Label>
-                    <Form.Control type="text" placeholder="Név" />
+                    <div className="to_center">
+                        <Button
+                            className="mybutton"
+                            variant="primary"
+                            type="submit"
+                        >
+                            Felvétel
+                        </Button>
+                    </div>
+                </Form>
+            </div>
 
-                </Form.Group>
-
-                <Form.Group className="mb-3 form_group">
-                    <Form.Label>Szak rövidítése</Form.Label>
-                    <Form.Control type="text" placeholder="Rövidítés" />
-                </Form.Group>
-
-                <div className="to_center">
-                    <Button className="mybutton" variant="primary" type="submit">
-                        Felvétel
-                    </Button>
-                </div>
-            </Form>
-        </div>
-
-        <div>
-            <MajorsTable></MajorsTable>
-        </div>
-
-    </main>
+            <div>
+                <MajorsTable></MajorsTable>
+            </div>
+        </main>
+    );
 }
